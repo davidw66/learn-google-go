ca44deeae51f6e83f84af72b378337d67795d736
David Williamson <davidw@pop-os.localdomain>
2023-12-28 08:26:42 -0700

    adding 30

---
+++ 30/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+
+	for i := 0; i < 42; i++ {
+
+		x := rand.Intn(5)
+
+		switch x {
+		case 0:
+			fmt.Printf("%v. x is %v\n", i, x)
+		case 1:
+			fmt.Printf("%v. x is %v\n", i, x)
+		case 2:
+			fmt.Printf("%v. x is %v\n", i, x)
+		case 3:
+			fmt.Printf("%v. x is %v\n", i, x)
+		case 4:
+			fmt.Printf("%v. x is %v\n", i, x)
+		default:
+			fmt.Println("Something went wrong")
+		}
+	}
+}
