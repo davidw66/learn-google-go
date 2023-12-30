818d4444fbacc8b16dfc46c178dc61a5130dabef
David Williamson <davidw@pop-os.localdomain>
2023-12-30 08:56:52 -0700

    Change to temp file

--- temp/main.go
+++ temp/main.go
@@ -1,39 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-)
+import "fmt"
 
 func main() {
-	x := rand.Intn(10)
-	y := rand.Intn(10)
-
-	fmt.Printf("x and y have values of %v and %v and the case met is ", x, y)
-
-	if x < 4 && y < 4 {
-		fmt.Println("x and y are both less than 4")
-	} else if x > 6 && y > 6 {
-		fmt.Println("x and y are both greater than 6")
-	} else if x >= 4 && x <= 6 {
-		fmt.Println("x is greater than or equal to 4 and less than or equal to 6")
-	} else if y != 5 {
-		fmt.Println("y is not equal to 5")
-	} else {
-		fmt.Println("none of the previous cases were met")
-	}
-
-	switch {
-	case x < 4 && y < 4:
-		fmt.Println("x and y are both less than 4")
-	case x > 6 && y > 6:
-		fmt.Println("x and y are both greater than 6")
-	case x >= 4 && x <= 6:
-		fmt.Println("x is greater than or equal to 4 and less than or equal to 6")
-	case y != 5:
-		fmt.Println("y is not equal to 5")
-	default:
-		fmt.Println("none of the previous cases were met")
-
-	}
+	a := [...]int{10, 12, 14, 16, 18}
+	fmt.Println(a)
+	fmt.Println(len(a))
+	fmt.Println(cap(a))
 }
