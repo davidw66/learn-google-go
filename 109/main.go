5684560a484aaf904306f3d6f04667d2f21f6629
David Williamson <davidw@pop-os.localdomain>
2024-01-01 10:42:48 -0700

    Example of slice and ranging over it

--- 109/main.go
+++ 109/main.go
@@ -3,7 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	// xi := []int{}
 	xi := make([]int, 0, 100)
 	for c := 42; c <= 51; c++ {
 		xi = append(xi, c)
@@ -12,4 +11,5 @@ func main() {
 	for i, v := range xi {
 		fmt.Printf("Index position %d is of value %d and of type %T\n", i, v, v)
 	}
+	fmt.Println(xi)
 }
