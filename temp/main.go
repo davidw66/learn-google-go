a01aeb716c040cee2bbeeaaf1ad62e255d4a07cc
David Williamson <davidw@pop-os.localdomain>
2024-01-01 08:44:55 -0700

    new example in temp

--- temp/main.go
+++ temp/main.go
@@ -3,8 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	a := [...]int{10, 12, 14, 16, 18}
-	fmt.Println(a)
-	fmt.Println(len(a))
-	fmt.Println(cap(a))
+	a := []int{10, 12, 14, 16, 18}
+	b := a
+	fmt.Println("a", a)
+	fmt.Println("b", b)
+	fmt.Println("------------------")
+	// notice that a and b both change
+	a[0] = 4
+	fmt.Println("a", a)
+	fmt.Println("b", b)
+
+	fmt.Println("------------------")
+	c := []int{10, 12, 14, 16, 18}
+	d := make([]int, 5, 100)
+	copy(d, c)
+	fmt.Println("c", c)
+	fmt.Println("d", d)
+
+	fmt.Println("------------------")
+	// notice that only slice c changes
+	c[0] = 4
+	fmt.Println("c", c)
+	fmt.Println("d", d)
 }
