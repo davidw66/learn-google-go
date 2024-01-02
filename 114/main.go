a3ce457aed08572a9bdae6c97794ff889e1923ca
David Williamson <davidw@pop-os.localdomain>
2024-01-02 09:47:27 -0700

    Map with slice
    Example of multidimensional slice

---
+++ 114/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	xj := []string{"James", "Bond", "Shaken not stirred"}
+	xm := []string{"Miss", "Moneypenny", "I'm 008"}
+	xss := [][]string{xj, xm}
+
+	fmt.Println(xss)
+
+	for _, v := range xss {
+		fmt.Println(v)
+		for _, v2 := range v {
+			fmt.Println(v2)
+		}
+
+	}
+}
