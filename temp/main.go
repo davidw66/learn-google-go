071123a310c51b2f4a42a4a80440ceec9dbf977a
David Williamson <davidw@pop-os.localdomain>
2024-01-10 08:21:06 -0700

    changes to temp file

--- temp/main.go
+++ temp/main.go
@@ -2,59 +2,24 @@ package main
 
 import "fmt"
 
-type dogYears int
-
 func main() {
+	fmt.Println(factorial(4))
+	fmt.Println(loopFactorial(4))
 
-	var age dogYears = 4
-	fluffy := 4
-	age.dogAge(fluffy)
-	{
-		type person struct {
-			first string
-			last  string
-			age   int
-		}
-
-		type secretAgent struct {
-			person
-			ltk   bool
-			first string
-		}
-
-		sa1 := secretAgent{
-			person: person{
-				first: "James",
-				last:  "Bond",
-				age:   32,
-			},
-			ltk:   true,
-			first: "Ethan",
-		}
-
-		p1 := person{
-			first: "Jenny",
-			last:  "Moneypenny",
-			age:   22,
-		}
-
-		p3 := struct {
-			first string
-			last  string
-			age   int
-		}{
-			first: "David",
-			last:  "Williamson",
-			age:   31,
-		}
-
-		fmt.Println(sa1)
-		fmt.Println(p1)
-		fmt.Println(p3)
-		fmt.Println(sa1.first, sa1.first, sa1.person.first)
-	}
 }
 
-func (d dogYears) dogAge(i int) {
-	fmt.Println(int(d) * i)
+func factorial(i int) int {
+	if i == 0 {
+		return 1
+	}
+	return i * factorial(i-1)
+}
+
+func loopFactorial(i int) int {
+	n := 1
+	for i > 0 {
+		n *= i
+		i--
+	}
+	return n
 }
