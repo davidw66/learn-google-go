d07d483fe9ffc8ea0294889722e9c05d5d95f230
David Williamson <davidw@pop-os.localdomain>
2024-01-21 07:24:33 -0700

    Generics type contraint

--- 182/main.go
+++ 182/main.go
@@ -3,11 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(add(2, 3))
-	fmt.Println(add(4.7, 5.9))
+	fmt.Println(addT(2, 3))
+	fmt.Println(addT(4.7, 5.9))
 
 }
 
-func add[g int | float64](a, b g) g {
+// It is convention to use capital T but not necessary
+// nor is it necessary to name the function xxxT
+func addT[T int | float64](a, b T) T {
 	return a + b
 }
