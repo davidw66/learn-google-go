94b2e07a6e84c98a5e21211b52e511d557073a85
David Williamson <davidw@pop-os.localdomain>
2024-02-13 08:23:17 -0700

    Custom error and using assertion

--- 236/main.go
+++ 236/main.go
@@ -2,41 +2,25 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"math"
 )
 
-type sqrtError struct {
-	err error
+type customErr struct {
+	errInfo string
 }
 
-func (sq sqrtError) Error() string {
-	return fmt.Sprintf("Sqrt error: %v", sq.err)
+func (ce customErr) Error() string {
+	return fmt.Sprintf("This is a custom error: %v", ce.errInfo)
 }
 
 func main() {
-	fmt.Println("Input any number")
-	fmt.Print(">-")
-
-	var f float64
-	_, err := fmt.Scan(&f)
-	if err != nil {
-		log.Println("You must input a number", err)
-		return
+	ce := customErr{
+		errInfo: "this is the custom error description",
 	}
-
-	sf, err := sqrt(f)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	fmt.Printf("The square root of %v is %v\n", f, sf)
+	foo(ce)
 }
 
-func sqrt(f float64) (float64, error) {
-	if f < 0 {
-		sre := fmt.Errorf("cannot take square root of a negative number %v", f)
-		return 0, sqrtError{sre}
-	}
-	return math.Sqrt(f), nil
+func foo(e error) {
+	fmt.Println("foo ran - ", e)
+	// Using assertion to print ce.errInfo
+	fmt.Println("Using assertion - ", e.(customErr).errInfo)
 }
