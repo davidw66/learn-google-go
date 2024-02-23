fba11634fd34c150565dbe5818d7ed2d81861a0c
David Williamson <davidw@pop-os.localdomain>
2024-02-23 08:50:13 -0700

    Factorial challenge solved

--- 274/main.go
+++ 274/main.go
@@ -4,13 +4,23 @@ import "fmt"
 
 func main() {
 	f := factorial(4)
-	fmt.Println("Total", f)
+
+	for n := range f {
+		fmt.Println("Total", n)
+	}
 }
 
-func factorial(n int) int {
+func factorial(n int) chan int {
+	c := make(chan int)
 	total := 1
+
 	for i := n; i > 0; i-- {
 		total *= i
 	}
-	return total
+
+	go func() {
+		c <- total
+		close(c)
+	}()
+	return c
 }
